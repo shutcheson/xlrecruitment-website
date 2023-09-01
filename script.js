758589a1e85282f8e04c5b59b227b15c23802b06
shutcheson <s.hutcheson06@gmail.com>
2023-09-01 17:01:14 +1000

    edited padding made the scroll finish earlier so all sectors were in the middle of the screen

--- script.js
+++ script.js
@@ -4,15 +4,27 @@ document.addEventListener("DOMContentLoaded", function () {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
 
-      const target = document.querySelector(this.getAttribute("href"));
+      const targetId = this.getAttribute("href").substring(1);
+      const target = document.getElementById(targetId);
+
       if (target) {
         target.style.scrollBehavior = "smooth";
-        target.scrollIntoView({
+
+        // Calculate the target scroll position
+        const targetOffset =
+          target.getBoundingClientRect().top + window.scrollY;
+        const adjustedOffset = targetOffset - 80;
+
+        // Scroll to the adjusted position
+        window.scrollTo({
+          top: adjustedOffset,
           behavior: "smooth",
-          block: "start", 
-          inline: "nearest",
         });
-        target.style.scrollBehavior = "auto";
+
+        // Reset scroll behavior after scrolling
+        setTimeout(() => {
+          target.style.scrollBehavior = "auto";
+        }, 500); // You can adjust the delay time as needed
       }
     });
   });
